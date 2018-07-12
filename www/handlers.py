d326084f06d822d7e7cd666f13125105c1bef9eb
0Accelerator0 <948386361@qq.com>
2018-07-12 16:08:59 +0800

    fix bug, succeed showing pages

--- www/handlers.py
+++ www/handlers.py
@@ -10,19 +10,23 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-async def index(request):
+def index(request):
 	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-	blogs = {
+	blogs = [
 		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
 		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
 		Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-	}
-	# users = await User.findAll()
+	]
 	return {
 		'__template__': 'blogs.html',
 		'blogs': blogs
 	}
-
-
+	
+@get('/api/users')
+def api_get_users():
+	users = yield from User.findAll(orderBy='created_at desc')
+	for u in users:
+		u.passwd = '******'
+	return dict(users=users)
 
 
