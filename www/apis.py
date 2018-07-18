95260c65dfd28f5297f0a6e3f0bf93a5729d2434
0Accelerator0 <948386361@qq.com>
2018-07-18 17:00:16 +0800

    complete function: comment, blog, and user

--- www/apis.py
+++ www/apis.py
@@ -77,12 +77,12 @@ class APIValueError(APIError):
 	def __init__(self, field, message=''):
 		super(APIValueError, self).__init__('value:invalid', field, message)
 		
-class APIResourceError(APIError):
+class APIResourceNotFoundError(APIError):
 	'''
 	Indicate the resource was not found. The data specifies the resource name.
 	'''
 	def __init__(self, field, message=''):
-		super(APIResourceError, self).__init__('value:notfound', field, message)
+		super(APIResourceNotFoundError, self).__init__('value:notfound', field, message)
 	
 class APIPermissionError(APIError):
 	'''
